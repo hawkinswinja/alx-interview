7e3efebd12b38a29cc62534b2d97d95581779c59
hawkinswinja <104626373+hawkinswinja@users.noreply.github.com>
2023-03-30 20:19:33 +0300

    Updated solution to task 0-lockboxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,19 @@
+def canUnlockAll(boxes):
+    """Checks if all boxes in mylist can be unlocked"""
+    opened = [0]
+    n =len(boxes)
+    for key in range(1, n):
+        if key in boxes[0]:
+            opened.append(key)
+            boxes[0] += boxes[key]
+    if len(opened) == n:
+        return True
+    for i in range(1,n):
+        if i in opened:
+            continue
+        if i in set(boxes[0]):
+            opened.append(i)
+            boxes[0] += boxes[i]
+        else:
+            return False
+    return True
