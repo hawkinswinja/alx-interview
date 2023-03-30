d0c76d778b7392fe70c06bffec6582cc817cfea7
hawkinswinja <hawkinswinja@gmail.com>
2023-03-31 01:58:48 +0300

    PEP8 standard vlaidation

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -5,14 +5,14 @@
 def canUnlockAll(boxes):
     """Checks if all boxes in mylist can be unlocked"""
     opened = [0]
-    n =len(boxes)
+    n = len(boxes)
     for key in range(1, n):
         if key in boxes[0]:
             opened.append(key)
             boxes[0] += boxes[key]
     if len(opened) == n:
         return True
-    for i in range(1,n):
+    for i in range(1, n):
         if i in opened:
             continue
         if i in set(boxes[0]):
