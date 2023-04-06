95ec50a298c316768b0bdc5a30dc999bfce699f8
hawkinswinja <hawkinswinja@gmail.com>
2023-04-06 22:52:38 +0000

    code cleanup for pep8

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 """This module contains a single function minOperations"""
 
+
 def minOperations(n):
     """Calculate the lowest no. of steps to get H"""
     cp = 2
     steps = 0
-    #check for edge cases
+    # check for edge cases
     while n > 1:
         while n % cp == 0:
             steps += cp
