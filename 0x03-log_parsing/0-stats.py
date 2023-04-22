c46d083e7997c9cee50629d073f4ad66a5fc25dd
hawkinswinja <104626373+hawkinswinja@users.noreply.github.com>
2023-04-22 20:44:26 +0300

    updated regex fullmatch

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -14,23 +14,22 @@ def display(fs, mydict):
 n = 0
 filesize = 0
 mydict = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
-
+date =  '\[\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\.\d+\]'
+protocol = ' "GET /projects/260 HTTP/1.1"'
+target = '.+ - ' + date + protocol + ' (?P<code>\d{3})' + ' (?P<size>\d+)'
 try:
     for line in sys.stdin:
         n += 1
-        if not re.search(r'^(\d{1,3}\.){3}\d{1,3} - ', line):
-            continue
-        log = line.split(' ')[-2:]
-        filesize += int(log[-1])
+        data = re.match(target, line)
+        if data:
+            data = data.groupdict()
+            filesize += int(data.get('size'))
         try:
-            mydict[int(log[0])] += 1
+            mydict[int(data.get('code'))] += 1
         except Exception:
             pass
         if n == 10:
             display(filesize, mydict)
             n = 0
-except Exception:
-    pass
-finally:
+except KeyboardInterrupt:
     display(filesize, mydict)
-    
