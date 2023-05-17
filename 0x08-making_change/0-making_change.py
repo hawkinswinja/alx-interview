dc5b24d9c917abab6b75b270fc9033d239e40aea
hawkinswinja <104626373+hawkinswinja@users.noreply.github.com>
2023-05-17 20:25:46 +0300

    making change

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""making_change module
+"""
+
+
+def makeChange(coins, total):
+    """returns fewest possible number of coins
+       Args:
+           coins: list of coins at hand
+           total: total amount to make change
+    """
+    if not total or len(coins) <= 0:
+        return 0
+    coins.sort(reverse=True)
+    num = 0
+    for coin in coins:
+        if coin <= total:
+            num += total // coin
+            total = total % coin
+    if total:
+        return -1
+    return num
