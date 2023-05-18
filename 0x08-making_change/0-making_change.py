e38cf65cd000a86d1b468e782252a12713e8c91b
hawkinswinja <104626373+hawkinswinja@users.noreply.github.com>
2023-05-18 20:21:38 +0300

    making change using greedy algorithm

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -9,14 +9,14 @@ def makeChange(coins, total):
            coins: list of coins at hand
            total: total amount to make change
     """
-    if not total or len(coins) <= 0:
+    if total <= 0:
         return 0
-    coins.sort()
-    num = [float('inf')] * (total + 1)
-    num[0] = 0
+    coins.sort(reverse=True)
+    num = 0
     for coin in coins:
-        for i in range(coin, total + 1):
-            num[i] = min(num[i], num[i - coin] + 1)
-    if num[total] == float('inf'):
+        if coin <= total:
+            num += total // coin
+            total = total % coin
+    if total != 0:
         return -1
-    return num[total]
+    return num
