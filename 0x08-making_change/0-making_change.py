a34700d79698b2bad5d0e864ed9764713ef050ae
hawkinswinja <104626373+hawkinswinja@users.noreply.github.com>
2023-05-18 08:28:01 +0300

    update to use dynamic programming

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -11,12 +11,12 @@ def makeChange(coins, total):
     """
     if not total or len(coins) <= 0:
         return 0
-    coins.sort(reverse=True)
-    num = 0
+    coins.sort()
+    num = [float('inf')] * (total + 1)
+    num[0] = 0
     for coin in coins:
-        if coin <= total:
-            num += total // coin
-            total = total % coin
-    if total:
+        for i in range(coin, total + 1):
+            num[i] = min(num[i], num[i - coin] + 1)
+    if num[total] == float('inf'):
         return -1
-    return num
+    return num[total]
